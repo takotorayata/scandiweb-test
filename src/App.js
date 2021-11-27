9cf29bca2a63d2fe01a290b2c44341ded9d888da
Tata <tatarusetskaya@gmail.com>
2021-11-27 16:09:24 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-
 import Form from './forms/Form';
 import Products from './forms/Products';
 
