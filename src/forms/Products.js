ff3ba5e50670dc4834c14128631b4b9da16b252f
Tata <tatarusetskaya@gmail.com>
2021-11-27 15:58:58 +0200

    Update Products.js

--- src/forms/Products.js
+++ src/forms/Products.js
@@ -1,40 +1,75 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Product from './Product';
 
-class Product extends Component {
+class Products extends Component {
 	constructor(props) {
 		super(props);
-		this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+		this.state = {
+			data: [],
+			deleteInfo: []
+		};
+		this.handler = this.handler.bind(this);
 	}
-	handleCheckboxChange() {
-		this.props.handler(this.props.data.sku);
+	componentDidMount() {
+		setTimeout(() => {
+			this.getProducts();
+		}, 500);
+	}
+	getProducts() {
+		axios
+			.get('https://scandiwebshop.000webhostapp.com/all-products.php')
+			.then((res) => this.setState({ data: res.data.products }));
+	}
+	deleteSelected() {
+		const deleteInfo = {
+			info: this.state.deleteInfo
+		};
+		axios
+			.post(
+				'https://scandiwebshop.000webhostapp.com/delete-products.php',
+				JSON.stringify(deleteInfo)
+			)
+			.then((res) => this.setState({ deleteInfo: [] }))
+			.finally(() => this.getProducts());
+	}
+	handler(sku) {
+		let newDeleteArray = this.state.deleteInfo.slice();
+		if (newDeleteArray.includes(sku)) {
+			newDeleteArray = newDeleteArray.filter((item) => {
+				return item !== sku;
+			});
+		} else {
+			newDeleteArray.push(sku);
+		}
+		this.setState({
+			deleteInfo: newDeleteArray
+		});
 	}
 
 	render() {
-		const p = this.props.data;
-
+		const products = this.state.data;
 		return (
 			<div>
-				<h2>{p.name}</h2>
-				<div className="body">
-					<p>SKU: {p.sku}</p>
-					<p>PRICE: ${p.price}</p>
-					{p.type === 'Book' && <p>WEIGHT: kg{p.weight}</p>}
-					{p.type === 'DVD' && <p>SIZE: MB {p.size}</p>}
-					{p.type === 'Furniture' && (
-						<p>
-							DIM: cm {p.height}x{p.length}x{p.width}
-						</p>
-					)}
+				<div style={{ display: 'flex' }}>
+					<button
+						className="form_button"
+						style={{ marginLeft: 'auto' }}
+						onClick={(e) => this.deleteSelected(e)}>
+						MASS DELETE
+					</button>
 				</div>
-				<div className="delete-checkbox">
-					Delete:
-					<input
-						type="checkbox"
-						onChange={this.handleCheckboxChange}
-					/>
-				</div>
+				<ul className="listing">
+					{products.map((item) => (
+						<div key={item.sku}>
+							<li>
+								<Product data={item} handler={this.handler} />
+							</li>
+						</div>
+					))}
+				</ul>
 			</div>
 		);
 	}
 }
-export default Product;
+export default Products;
