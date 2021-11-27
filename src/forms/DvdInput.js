a4c4ca608eb1fa2fd0c4bffb19827dd42f924c5f
Tata <tatarusetskaya@gmail.com>
2021-11-27 15:43:38 +0200

    Update DvdInput.js

--- src/forms/DvdInput.js
+++ src/forms/DvdInput.js
@@ -1,23 +1,25 @@
 const DvdInput = (props) => {
-    if (props.additional === 'DVD') {
-        return (
-            <div>
-                <label className="tooltip" htmlFor={props.name}>{props.title}
-                    <span className="tooltiptext">{props.tooltip}</span>
-                </label>
-                <input
-                    id={props.id}
-                    name={props.name}
-                    type={props.type}  onKeyDown={ (e) => (e.key === '.' || e.key ==='e') && e.preventDefault() }
-                    value={props.value}
-                    onChange={props.handleChange}
-                    placeholder={props.placeholder}
-                    required
-                />
-            </div>
-        )
-    } else {
-        return <div></div>
-    }
-}
+	if (props.additional === 'DVD') {
+		return (
+			<div>
+				<label className="tooltip" htmlFor={props.name}>
+					{props.title}
+					<span className="tooltiptext">{props.tooltip}</span>
+				</label>
+				<input
+					id={props.id}
+					name={props.name}
+					type={props.type}
+					onKeyDown={(e) =>
+						(e.key === '.' || e.key === 'e') && e.preventDefault()
+					}
+					value={props.value}
+					onChange={props.handleChange}
+					placeholder={props.placeholder}
+					required
+				/>
+			</div>
+		);
+	} else return <div></div>;
+};
 export default DvdInput;
