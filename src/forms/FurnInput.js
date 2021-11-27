c22dad9cb6d070f3b0eb581fd2ee997f2944125a
Tata <tatarusetskaya@gmail.com>
2021-11-27 15:46:32 +0200

    Create FurnInput.js

---
+++ src/forms/FurnInput.js
@@ -0,0 +1,25 @@
+const FurnInput = (props) => {
+	if (props.additional === 'Furniture') {
+		return (
+			<div>
+				<label className="tooltip" htmlFor={props.name}>
+					{props.title}
+					<span className="tooltiptext">{props.tooltip}</span>
+				</label>
+				<input
+					id={props.id}
+					name={props.name}
+					type={props.type}
+					onKeyDown={(e) =>
+						(e.key === '.' || e.key === 'e') && e.preventDefault()
+					}
+					value={props.value}
+					onChange={props.handleChange}
+					placeholder={props.placeholder}
+					required
+				/>
+			</div>
+		);
+	} else return <div></div>;
+};
+export default FurnInput;
