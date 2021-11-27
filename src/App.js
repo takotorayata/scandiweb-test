04e5733d0ddf2b503c84965010b236aeaf2351b6
Tata <tatarusetskaya@gmail.com>
2021-11-27 16:09:06 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Form from './forms/Form';
+import Products from './forms/Products';
+
+export default function App() {
+	return (
+		<div id="container">
+			<Router>
+				<div>
+					<Link to="/">
+						<button className="form_button">All products</button>
+					</Link>
+					<Link to="/add-product">
+						<button className="form_button">ADD</button>
+					</Link>
+					<Switch>
+						<Route exact path="/">
+							<Products />
+						</Route>
+						<Route path="/add-product">
+							<Form />
+						</Route>
+					</Switch>
+				</div>
+			</Router>
+		</div>
+	);
+}
